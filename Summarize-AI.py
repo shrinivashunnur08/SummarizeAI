9937ceec691efca109305f5f66c69a53b31053e4
Shrinivas Hunnur <135942833+shrinivashunnur08@users.noreply.github.com>
2024-06-30 11:44:15 +0530

    Add files via upload

--- Summarize-AI.py
+++ Summarize-AI.py
@@ -8,7 +8,10 @@ load_dotenv()  # Load all the environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-prompt = """You are a YouTube video summarizer. Your task is to read the provided transcript and create a concise summary of the video's content. Please condense the key points and main ideas into a list of bullet points, ensuring the summary can longer than 500-1500 words. Use clear and precise language."""
+prompt = """
+You are a YouTube video summarizer. Your task is to read the provided transcript and create a concise summary of the video's content in English. Please condense the key points and main ideas into a list of bullet points, ensuring the summary is no longer than 500-1500 words. Use clear and precise language. 
+Note: Even if the transcript is in Hindi, Marathi, Tamil, Kannada, or any other language, the summary should be in English.
+"""
 
 # Function to get transcript in the specified language
 def extract_transcript_details(youtube_video_url, language='en'):
@@ -30,14 +33,20 @@ def extract_transcript_details(youtube_video_url, language='en'):
 # Function to get summary from Google Gemini Pro
 def generate_gemini_content(transcript_text, prompt):
     model = genai.GenerativeModel("gemini-pro")
-    response = model.generate_content(prompt + transcript_text)
+    response = model.generate_content(prompt + "\n\n" + transcript_text)
     return response.text
 
 st.title("Welcome to Shree's Summarize-AI App 🤖")
 youtube_link = st.text_input("Enter the YouTube video link: (Ensure the URL contains characters up to the '&' symbol, but does not include the '&' itself)")
 
-language = st.selectbox("Select Transcript Language", ["English", "Hindi"])
-language_code = 'en' if language == 'English' else 'hi'
+language = st.selectbox("Select Transcript Language", ["English", "Hindi", "Marathi", "Tamil", "Kannada"])
+language_code = {
+    'English': 'en',
+    'Hindi': 'hi',
+    'Marathi': 'mr',
+    'Tamil': 'ta',
+    'Kannada': 'kn'
+}[language]
 
 if youtube_link:
     video_id = youtube_link.split("=")[1]
@@ -51,4 +60,4 @@ if st.button("Summarize 📃"):
         st.markdown("## Summarize AI says:")
         st.write(summary)
     else:
-        st.write("Oops, There is no transcript available for the selected language 😕")
+        st.write("No transcript available for the selected language.")
